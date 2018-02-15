2b50b460df370476aa0c27a2c3ae7e598b0620db
eda <kimmirindel97@gmail.com>
2018-02-16 11:49:08 +1300

    web API working

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
 
-const redditRoutes = require('./reddit')
+const feedMeRoutes = require('./feedMeRoute')
 const server = express()
+server.use(bodyParser.urlencoded({extended: false}))
 
 server.use(express.static(path.join(__dirname, '../public')))
 
-server.use('/api/v1/reddit', redditRoutes)
+server.use('/api/v1/feedme', feedMeRoutes)
 
 module.exports = server
